59aaa89dd99236af6624da759cfa291b6e5d75a9
Pal <palakvasara9199@gmail.com>
2019-06-13 21:26:14 -0700

    models.py file is ready for both apps(posts & groups)

--- groups/models.py
+++ groups/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+# hepls to remove extra characters like alphanumeric or underscore or hyphens
+# the use of this is if you have string and want to use as URL then it`s going to remove spaces and add dashes ...
+from django.utils.text import slugify
+# install misaka in terminal
+import misaka
+
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+# this is how we can use custom template tag
+from django import template
+register = template.Library()
+
 
 # Create your models here.
+class Group(models.Model):
+    name = models.CharField(max_length=255,unique=True)
+    slug = models.SlugField(allow_unicode=True,unique=True)
+    description = models.TextField(blank=True,default='')
+    description_html = models.TextField(editable=False,default='',blank=True)
+    members = models.ManyToManyField(User,through='GroupMember')
+
+    def __str__(self):
+        return self.name
+
+    def save(self,*args,**kwargs):
+        self.slug = slugify(self.name)
+        self.description_html = misaka.html(self.description)
+        super().save(*args,**kwargs)
+
+    def get_absolute_url(self):
+        return reverse('groups:single',kwargs={'slug':self.slug})
+
+    class Meta:
+        ordering = ['name']
+
+
+class GroupMember(models.Model):
+        group = models.ForeignKey(Group, related_name='memberships')
+        user = models.ForeignKey(User,related_name='user_groups')
+
+        def __str__(self):
+            return self.user.username
+
+
+        class Meta:
+            unique_together = ('group','user')
